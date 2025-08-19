7e023e271627e91e1031d0079a98bf1554c02b47
paithiov909 <am11.44@gmail.com>
2025-08-19 12:19:47 +0900

    tidy

--- inst/include/nativeshadr.h
+++ inst/include/nativeshadr.h
@@ -16,26 +16,28 @@ inline uint32_t int4_to_icol(int4 col) {
 inline int4 texture_eval(const Rcpp::IntegerMatrix& nr,
                          const std::vector<int>& wh) {
   if (wh.size() != 2) {
-    throw std::runtime_error("hw must have length 2");
+    throw std::runtime_error("wh must have length 2");
   }
-  const auto dim = std::make_tuple(nr.nrow(), nr.ncol());
-  const auto i = std::clamp(wh[1], 0, std::get<0>(dim)); // height
-  const auto j = std::clamp(wh[0], 0, std::get<1>(dim)); // width
+  const auto dim = std::make_tuple(nr.nrow(), nr.ncol());  // height, width
+  const auto i = std::clamp(wh[1], 0, std::get<0>(dim));   // height
+  const auto j = std::clamp(wh[0], 0, std::get<1>(dim));   // width
   const int4 ret = icol_to_int4(nr[j + i * std::get<1>(dim)]);
   return ret;
 }
 
 inline std::function<Rcpp::IntegerVector(Rcpp::IntegerMatrix, Rcpp::List)>
 vectorize_shader(
-    std::function<uint32_t(Rcpp::IntegerMatrix, std::vector<int>, Rcpp::List)>
+    std::function<uint32_t(std::vector<int>, Rcpp::IntegerMatrix, Rcpp::List)>
         shader) {
   return [shader](Rcpp::IntegerMatrix nr, Rcpp::List uniforms) {
-    const auto dim = std::make_tuple(nr.nrow(), nr.ncol()); // height, width
-    Rprintf("dim: %d, %d\n", std::get<0>(dim), std::get<1>(dim));
+    const auto dim = std::make_tuple(nr.nrow(), nr.ncol());  // height, width
+    Rprintf("dim (height, width): %d, %d\n", std::get<0>(dim),
+            std::get<1>(dim));
     Rcpp::IntegerVector ret(std::get<0>(dim) * std::get<1>(dim));
-    for (int i = 0; i < std::get<0>(dim); i++) { // height
-      for (int j = 0; j < std::get<1>(dim); j++) { // width
-        ret[j + i * std::get<1>(dim)] = shader(nr, std::vector<int>{j, i}, uniforms);
+    for (int i = 0; i < std::get<0>(dim); i++) {    // height
+      for (int j = 0; j < std::get<1>(dim); j++) {  // width
+        ret[j + i * std::get<1>(dim)] =
+            shader(std::vector<int>{j, i}, nr, uniforms);
       }
     }
     ret.attr("dim") = Rcpp::IntegerVector{std::get<0>(dim), std::get<1>(dim)};
--- src/nativeshadr.cpp
+++ src/nativeshadr.cpp
@@ -4,15 +4,17 @@ using namespace hlslpp;
 
 // [[Rcpp::export]]
 Rcpp::IntegerVector test_as_is(Rcpp::IntegerMatrix nr, Rcpp::List uniforms) {
-  auto as_is = [](Rcpp::IntegerMatrix nr, std::vector<int> xy, Rcpp::List uniforms) {
+  auto as_is = [](std::vector<int> xy, Rcpp::IntegerMatrix nr,
+                  Rcpp::List uniforms) {
     int4 col = texture_eval(nr, xy);
     return int4_to_icol(col);
   };
   return vectorize_shader(as_is)(nr, uniforms);
 }
 
-uint32_t texture(Rcpp::IntegerMatrix nr, std::vector<int> hw, Rcpp::List uniforms) {
-  float2 uv = float2(hw[0], hw[1]) / float2(nr.ncol(), nr.nrow());
+uint32_t texture(std::vector<int> xy, Rcpp::IntegerMatrix nr,
+                 Rcpp::List uniforms) {
+  float2 uv = float2(xy[0], xy[1]) / float2(nr.ncol(), nr.nrow());
   float4 col = float4(uv.x, uv.y, .6, 1);
   return int4_to_icol(clamp(col * 255, 0, 255));
 }
